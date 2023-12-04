4bc635d1a21336b7c2ae6cd201ce4eefaf7818fd
MinhHieuVux <minhhieuvux@gmail.com>
2023-12-04 13:47:56 +0700

    update

--- src/firebaseScript.js
+++ src/firebaseScript.js
@@ -1,6 +1,6 @@
 // firebaseScript.js
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import {getDatabase, set, ref,get, update} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import {getDatabase, set, ref, get, update} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
 
 
 var firebaseConfig = {
@@ -17,13 +17,13 @@ var firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 console.log(database)
-const locationRef = ref(database, "latitude");
-const locationRef2 = ref(database, "longitude");
-console.log('latitude' + locationRef)
-console.log('longitude' + locationRef)
+const locationRef = ref(database, "heartRate");
+const locationRef2 = ref(database, "breathRate");
+const locationRefList = ref(database, "data_save");
+console.log('heartRate' + locationRef)
+console.log('breathRate' + locationRef)
 
 async function fetchData() {
-    var locationData = [];
 
     try {
         const snapshot1 = await get(locationRef);
@@ -31,22 +31,36 @@ async function fetchData() {
             const heartRateData = snapshot1.val();
             console.log('Data - Latitude:', heartRateData);
             document.getElementById('heartRate').innerHTML = `Nhịp Tim: ${heartRateData} bpm`;
-            locationData.unshift(heartRateData)
+            const snapshot2 = await get(locationRef2);
+            if (snapshot2.val()) {
+                const breathRateData = snapshot2.val();
+                console.log('Data - Longitude:', breathRateData);
+                document.getElementById('breathRate').innerHTML = `SPO2: ${breathRateData} %`;
+                // locationData.unshift(breathRateData)
+                const snapshot3 = await get(locationRefList);
+                const dataList = snapshot3.val() || []
+                console.log('dsadfadsf', dataList);
+
+                function startMeasurement() {
+                    const newData = {
+                        heartRate: 84,
+                        breathRate: 21.50
+                    };
+                    dataList.push(newData);
+                    set(locationRefList, dataList);
+                }
+
+                document.getElementById('startButton').onclick = startMeasurement;
+            } else {
+                console.log('Null data - Longitude.');
+            }
+
+            // console.log('Location Data:', locationData);
+            // locationData.unshift(heartRateData)
         } else {
             console.log('Null data - Latitude.');
         }
 
-        const snapshot2 = await get(locationRef2);
-        if (snapshot2.val()) {
-            const breathRateData = snapshot2.val();
-            console.log('Data - Longitude:', breathRateData);
-            document.getElementById('breathRate').innerHTML = `Sp02: ${breathRateData} %`;
-            locationData.unshift(breathRateData)
-        } else {
-            console.log('Null data - Longitude.');
-        }
-
-        console.log('Location Data:', locationData);
     } catch (error) {
         console.error('Error:', error);
     }
